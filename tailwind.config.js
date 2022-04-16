a95003ce92483a1e5565380d8893a10b6a26cae5
skykanin <3789764+skykanin@users.noreply.github.com>
2022-04-16 23:44:03 +0200

    Make colour theme WCAG compliant

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,9 @@ module.exports = {
         'purple-navy': '#4e5283',
         'tuscany': '#cca7a2',
         'mauve' : '#d9bbf9',
-        'dark-blue-gray': '#7871aa',
+        'rebecca-purple': '#9c93de',
         'jet': '#323234',
-        'crimson-ua': '#941c2f',
-        'dark-salmon': '#F89577',
+        'st-patricks-blue': '#222a68',
       }
     },
   },

19390060027f5bf60d1aa8240a8f6c80ff59bc32
skykanin <3789764+skykanin@users.noreply.github.com>
2022-05-15 20:16:14 +0200

    Add HLS tool to dev env and update lock file

--- flake.nix
+++ flake.nix
@@ -5,29 +5,44 @@
 
     # Utility library for backwards compatibility with the old Nix CLI.
     flake-compat = {
-      url = "github:edolstra/flake-compat";
+      url =
+        "github:edolstra/flake-compat?rev=b4a34015c698c7793d592d66adbab377907a2be8";
       flake = false;
     };
 
     # Unofficial library of utilities for managing Nix Flakes.
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.url =
+      "github:numtide/flake-utils?rev=12806d31a381e7cd169a6bac35590e7b36dc5fe5";
 
     nixpkgs.url =
       "github:nixos/nixpkgs?rev=bef07673d323a9c489a664ba7dee3dd10468a293";
 
-    # TODO: Provide HLS from their flake
+    # The Haskell Language Server IDE tooling.
+    hls = {
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+      url =
+        "github:haskell/haskell-language-server?rev=b1bf5499155e259341e4868863b0fd743b6bd65f";
+    };
   };
 
-  outputs = { self, flake-compat, flake-utils, nixpkgs, ... }:
+  outputs = { self, flake-compat, flake-utils, hls, nixpkgs }:
     flake-utils.lib.eachSystem [
       "x86_64-linux"
       "x86_64-darwin"
-      "aarch64-linux"
+      #TODO: HLS' flake doesn't support this arch for some reason, perhaps create an issue for this
+      #"aarch64-linux"
       "aarch64-darwin"
     ] (system:
       let
+        lib = nixpkgs.lib;
         compiler-version = "ghc922";
         pkgs = nixpkgs.legacyPackages.${system};
+        haskell-language-server =
+          hls.packages.${system}."haskell-language-server-${
+            lib.removePrefix "ghc" compiler-version
+          }";
       in {
         # Setup development environment
         devShells.default = let
@@ -42,9 +57,10 @@
             hs.cabal-install
             hs.ghcid
             hs.fourmolu
+            haskell-language-server
           ];
           libraries = with pkgs; [ zlib ];
-          libraryPath = "${pkgs.lib.makeLibraryPath libraries}";
+          libraryPath = "${lib.makeLibraryPath libraries}";
         in pkgs.mkShell {
           buildInputs = tools ++ libraries;
           shellHook = ''
